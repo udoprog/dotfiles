14c80586231f6b73b92c6df7b780290d83219ddb
John-John Tedro <johnjohn.tedro@gmail.com>
2014-03-01 21:17:12 +0100

    Use Bundle for powerline.

--- build/powerline.mk
+++
@@ -1,9 +0,0 @@
-.PHONY: all
-
-all: vim/bundle/powerline/plugin
-
-vim/bundle/powerline: vim/bundle
-	mkdir -p $@
-
-vim/bundle/powerline/plugin: vim/bundle/powerline
-	ln -fs $(CURDIR)/powerline/powerline/bindings/vim/plugin $@
