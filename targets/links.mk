db7924fc256894c6f7319996280bac0467a7cb44
John-John Tedro <udoprog@tedro.se>
2017-08-02 13:09:54 +0200

    idea link

--- targets/links.mk
+++ targets/links.mk
@@ -3,10 +3,18 @@ HAS_NVIM := $(shell has-command nvim)
 build-$(HAS_NVIM) += $(BIN)/vim
 build-$(HAS_NVIM) += $(BIN)/view
 
+IDEA := $(HOME)/usr/idea/bin/idea.sh
+HAS_IDEA := $(shell has-path $(IDEA))
+
+build-$(HAS_IDEA) += $(BIN)/idea
+
 $(BIN)/vim: /usr/bin/nvim
 	relative-ln $@ $<
 
 $(BIN)/view: /usr/bin/nvim
+	relative-ln $@ $<
+
+$(BIN)/idea: $(IDEA)
 	relative-ln $@ $<
 
 include $(ROOT)/lib.mk
