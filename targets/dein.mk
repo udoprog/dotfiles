9382278f72cdf710bd5f2c1fd234879a9d5943f2
John-John Tedro <udoprog@tedro.se>
2017-09-24 15:17:08 +0200

    [targets] harden dein target

--- targets/dein.mk
+++ targets/dein.mk
@@ -2,6 +2,10 @@ url := https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh
 
 steps += dein
 
-dein:
-	once dein "curl $(url) > $(CACHE)/dein-install.sh"
+dein: $(HOME)/.dein
+
+$(HOME)/.dein: $(CACHE)/dein-install.sh
 	sh $(CACHE)/dein-install.sh $(HOME)/.dein
+
+$(CACHE)/dein-install.sh:
+	curl $(url) > $(CACHE)/dein-install.sh
