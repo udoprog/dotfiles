4cf725e3c05d34c95451e91e2756120cb47f49de
John-John Tedro <udoprog@tedro.se>
2018-01-01 19:53:27 +0100

    do configs after other things

--- dotfiles.mk
+++ dotfiles.mk
@@ -2,7 +2,6 @@ steps += $(CACHE)
 steps += $(ROOT)/.submodules
 steps += packages
 
-targets += targets/configs.mk
 targets += targets/dein.mk
 targets += targets/links.mk
 targets += targets/mutt.mk
@@ -10,6 +9,7 @@ targets += targets/oh-my-zsh.mk
 targets += targets/rust.mk
 targets += targets/vim.mk
 targets += targets/vscode.mk
+targets += targets/configs.mk
 targets-$(has-systemd) += targets/reposync.mk
 
 once += jshint
