88e9738204ea05357ad235d75b0aa50058263d4a
John-John Tedro <udoprog@tedro.se>
2022-04-08 12:34:05 +0200

    Update config

--- home/.fzf.zsh
+++ home/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == *$HOME/.config/nvim/plugged/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$HOME/.config/nvim/plugged/fzf/bin"
+  export PATH="${PATH:+${PATH}:}$HOME/.local/share/nvim/plugged/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.config/nvim/plugged/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.local/share/nvim/plugged/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.config/nvim/plugged/fzf/shell/key-bindings.zsh"
+source "$HOME/.local/share/nvim/plugged/fzf/shell/key-bindings.zsh"
